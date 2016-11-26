454a17099b487d3cda94fd9590a712bd3aeef945
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-26 20:13:41 +0300

    добавлены классы исключений

---
+++ components/exceptions/FileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+class FileNotFoundException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct(
+            "File `$message` does not exist",
+            $code,
+            $previous
+        );
+    }
+}
