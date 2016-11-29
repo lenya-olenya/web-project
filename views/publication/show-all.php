d0f38640346af63b4f638299ad181acaac76743b
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-29 05:33:20 +0300

    обновлена вьюха для показа всех записей

--- views/publication/show-all.php
+++ views/publication/show-all.php
@@ -2,28 +2,56 @@
 
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <title>Title</title>
-</head>
+<?php
+
+require_once ROOT . '/templates/Head.php';
+require_once ROOT . '/templates/HeaderPanel.php';
+require_once ROOT . '/templates/Pagination.php';
+
+$head = new Head('Главная', Application::SITE_FONTS, Application::SITE_STYLES);
+$header = new HeaderPanel();
+$pagination = new Pagination();
+
+$pageCount = ceil($this->_publicationModel->getCount() / (float) $this->_publicationsPerPage);
+
+?>
+
+<?= $head->render() ?>
 
 <body>
 
-<?php foreach ($publications as $p): ?>
-  <article>
-    <header>
-      <a href="/publication/show/<?= $p['id'] ?>"><?= $p['title'] ?></a>
-    </header>
+<?= $header->render() ?>
 
-    <p>
-      <?= $p['description'] ?>
-    </p>
+<h1 class="title">Главная</h1>
 
-    <footer>
-      <?= $p['date'] ?>
-    </footer>
-  </article>
-<?php endforeach; ?>
+<main class="publication-list">
+  <ul class="article-previews">
+    <?php foreach ($publications as $p): ?>
+      <li>
+        <article class="article-preview">
+          <header class="title">
+            <a href="/publication/show/<?= $p['id'] ?>"><?= $p['title'] ?></a>
+          </header>
+
+          <p><?= $p['description'] ?></p>
+
+          <footer>
+            <span><?= $p['date'] ?></span> | Тема:
+            <span><?= $this->_themeModel->getName($p['theme_id']) ?></span>
+
+            <span class="more">
+              <a href="/publication/show/<?= $p['id'] ?>">Перейти к записи »</a>
+            </span>
+          </footer>
+        </article>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
+  <div style="text-align: center; clear: both">
+      <?= $pagination->render(['activePage' => $page, 'pageCount' => $pageCount]) ?>
+  </div>
+</main>
 
 </body>
 
