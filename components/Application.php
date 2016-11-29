7a28d07851539291ae13bff96919971a6b239491
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-29 05:28:13 +0300

    добавил новые константы - пути к файлам стилей для сайта

--- components/Application.php
+++ components/Application.php
@@ -8,6 +8,10 @@ class Application
 {
     const COOKIE_EXPIRE_SESSION = null;
     const COOKIE_EXPIRE_YEAR = 60 * 60 * 24 * 365;
+    const SITE_STYLES = '/resources/css/styles.css';
+    const SITE_FONTS =
+        'https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,' .
+        '400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin-ext';
 
     public static $config;
 
