a7ee45e1c8f78635133cf1b9fddfea76def66db4
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-29 06:19:24 +0300

    новое представление для записей конкретной категории

---
+++ views/publication/show-theme.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php
+
+require_once ROOT . '/templates/Head.php';
+require_once ROOT . '/templates/HeaderPanel.php';
+require_once ROOT . '/templates/Pagination.php';
+
+$title = $this->_themeModel->getName($themeId);
+
+$head = new Head($title, Application::SITE_FONTS, Application::SITE_STYLES);
+$header = new HeaderPanel();
+$pagination = new Pagination();
+
+$pageCount = ceil($this->_publicationModel->getCount() / (float) $this->_publicationsPerPage);
+
+?>
+
+<?= $head->render() ?>
+
+<body>
+
+<?= $header->render() ?>
+
+<h1 class="title"><?= $title ?></h1>
+
+<main class="publication-list">
+  <ul class="article-previews">
+    <?php foreach ($publications as $p): ?>
+      <li>
+        <article class="article-preview">
+          <header class="title">
+            <a href="/publication/show/<?= $p['id'] ?>"><?= $p['title'] ?></a>
+          </header>
+
+          <p><?= $p['description'] ?></p>
+
+          <footer>
+            <span><?= $p['date'] ?></span> | Тема:
+            <span><?= $this->_themeModel->getName($p['theme_id']) ?></span>
+
+            <span class="more">
+              <a href="/publication/show/<?= $p['id'] ?>">Перейти к записи »</a>
+            </span>
+          </footer>
+        </article>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
+  <div style="text-align: center; clear: both">
+    <?= $pagination->render(['activePage' => $page, 'pageCount' => $pageCount]) ?>
+  </div>
+</main>
+
+</body>
+
+</html>
