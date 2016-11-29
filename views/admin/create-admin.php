31c78d9c72527d25d63efa8de7091d7b2b7e45d4
Serjih Sklovski <serjihsklovski@gmail.com>
2016-11-29 06:42:39 +0300

    поправил вьюху регистрации нового админа

--- views/admin/create-admin.php
+++ views/admin/create-admin.php
@@ -2,48 +2,45 @@
 
 <html>
 
-<head>
-    <title>Регистрация</title>
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+<?php
 
-    <style>
-        @import "/resources/css/styles.css";
-    </style>
-</head>
+require_once ROOT . '/templates/Head.php';
+require_once ROOT . '/templates/HeaderPanel.php';
+
+$title = 'Регистрация нового администратора';
+
+$head = new Head('Регистрация', Application::SITE_FONTS, Application::SITE_STYLES);
+$headerPanel = new HeaderPanel();
+
+?>
+
+<?= $head->render() ?>
 
 <body>
 
-<header>
-    <ul>
-        <li><a href="#">Главная</a></li>
-        <li><a href="#">Товары</a></li>
-        <li><a href="#">Корзина</a></li>
-        <li><a href="#">О нас</a></li>
-    </ul>
-</header>
+<?= $headerPanel->render() ?>
 
-<h1 class="title">Регистрация нового администратора</h1>
+<h1 class="title"><?= $title; ?></h1>
 
-<form action="" method="post">
-    <ul>
-        <li>
-            <label for="login">Логин:</label>
-            <input type="text" name="login">
-        </li>
+<form action="" method="post" style="opacity: 0.8">
+  <ul>
+    <li>
+      <label for="login">Логин:</label>
+      <input type="text" name="login">
+    </li>
 
-        <li>
-            <label for="password">Пароль:</label>
-            <input type="password" name="password">
-        </li>
+    <li>
+      <label for="password">Пароль:</label>
+      <input type="password" name="password">
+    </li>
 
-        <li>
-            <label for="repeatPassword">Повторите пароль:</label>
-            <input type="password" name="repeatPassword">
-        </li>
-    </ul>
+    <li>
+      <label for="repeatPassword">Повторите пароль:</label>
+      <input type="password" name="repeatPassword">
+    </li>
+  </ul>
 
-    <button class="submit" type="submit">Отправить</button>
+  <button class="submit" type="submit">Отправить</button>
 </form>
 
 </body>
