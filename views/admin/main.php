2b4b04b51354658fa480d0c0f45111de9882d0b4
Serjih Sklovski <serjihsklovski@gmail.com>
2017-03-20 12:06:48 +0300

    updated

--- views/admin/main.php
+++ views/admin/main.php
@@ -1,15 +1,82 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html>
 
-<head>
-    <meta charset="UTF-8">
-    <title>AdminPanel</title>
-</head>
+<?php
+
+require_once ROOT . '/templates/Head.php';
+require_once ROOT . '/templates/HeaderPanel.php';
+
+$title = 'Панель администратора';
+
+$head = new Head($title);
+$headerPanel = new HeaderPanel();
+
+$head->addStyle(Application::SITE_FONTS);
+$head->addStyle(Application::SITE_STYLES);
+
+?>
+
+<?= $head->render() ?>
 
 <body>
 
-hello admin
+<?= $headerPanel->render() ?>
+
+<main>
+  <ul class="scroll-area">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+  </ul>
+</main>
+
+<!--hello admin-->
 
 </body>
 
--- views/admin/sign-in-admin.php
+++ views/admin/sign-in-admin.php
@@ -7,15 +7,14 @@
 require_once ROOT . '/templates/Head.php';
 require_once ROOT . '/templates/HeaderPanel.php';
 
-$title = 'Регистрация нового администратора';
+$title = 'Вход администратора';
 
-$head = new Head('Регистрация');
+$head = new Head($title);
 $headerPanel = new HeaderPanel();
 
 $head->addStyle(Application::SITE_FONTS);
 $head->addStyle(Application::SITE_STYLES);
 $head->addScript('/resources/js/jquery-1.12.4.js');
-//$head->addScript('/resources/js/password-matches.js');
 
 ?>
 
@@ -27,7 +26,7 @@ $head->addScript('/resources/js/jquery-1.12.4.js');
 
 <h1 class="title"><?= $title; ?></h1>
 
-<form id="signUpForm" action="/admin" method="post" style="opacity: 0.8">
+<form id="signInForm" action="/admin" method="post" style="opacity: 0.8">
   <ul>
     <li>
       <label for="login">Логин:</label>
@@ -37,11 +36,6 @@ $head->addScript('/resources/js/jquery-1.12.4.js');
     <li>
       <label for="password">Пароль:</label>
       <input type="password" name="password" id="password" required>
-    </li>
-
-    <li>
-      <label for="repeatPassword">Повторите пароль:</label>
-      <input type="password" name="confirmPassword" id="confirmPassword" required>
     </li>
   </ul>
 
